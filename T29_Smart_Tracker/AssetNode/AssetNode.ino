5bc4a5252421007508f2b5550e95ed42097cbee2
Zanaqi <2200981@sit.singaporetech.edu.sg>
2024-03-16 03:29:16 +0800

    can gather rssi regularly
    - although a bit slow so might need to optimize it

--- T29_Smart_Tracker/AssetNode/AssetNode.ino
+++ T29_Smart_Tracker/AssetNode/AssetNode.ino
@@ -4,25 +4,28 @@
 #define SERVICE_UUID "01234567-0123-4567-89ab-0123456789ab"
 #define bleServerName "AssetTrackingSystem"
 
-static BLEUUID positionCharacteristicUUID("01234567-0123-4567-89ab-0123456789cd");
+#define MAX_NODES 3
+
+static BLEUUID nodeCharacteristicUUID("01234567-0123-4567-89ab-0123456789cd");
 static BLEUUID serviceUUID(SERVICE_UUID);
 
 static boolean doConnect = false;
 static boolean connected = false;
 
 static BLEAddress *pServerAddress;
-static BLERemoteCharacteristic* positionCharacteristic;
-static BLEAddress *pServerAddresses[3];
+static BLERemoteCharacteristic* nodeCharacteristic;
+static BLEAddress *pServerAddresses[MAX_NODES];
 
 static BLEClient* pClient;
+static BLEClient* pNodes[MAX_NODES];
 
 const uint8_t notificationOn[] = {0x1, 0x0};
 const uint8_t notificationOff[] = {0x0, 0x0};
 
-char* positionStr;
-boolean newPosition = false;
+char* nodeStr;
+boolean newNode = false;
 
-// Known positions (coordinates) of corner nodes
+// Known nodes (coordinates) of corner nodes
 double xCornerNode1 = 0.0;
 double yCornerNode1 = 0.0;
 double xCornerNode2 = 5.0; // Example coordinates (adjust as per your setup)
@@ -31,18 +34,16 @@ double xCornerNode3 = 0.0;
 double yCornerNode3 = 5.0;
 
 // Array to store RSSI values from corner nodes
-int cornerNodeRssiArr[3];
+int cornerNodeRssiArr[MAX_NODES] = {0,0,0};
 
 // Variables to store distances from corner nodes to asset node
 double distanceCornerNode1 = 0.0;
 double distanceCornerNode2 = 0.0;
 double distanceCornerNode3 = 0.0;
 
-// Variables to store estimated position of asset node
+// Variables to store estimated node of asset node
 double xAssetNode = 0.0;
 double yAssetNode = 0.0;
-
-int cornerNodeRSSI = 0; // Variable to store corner node RSSI
 
 int currNodeIndex = 0;
 
@@ -52,7 +53,8 @@ std::string cornerNodes[] = {
   "CornerNode3"
 };
 
-bool isAllNodesDiscovered = false;
+int cornerNodesDiscovered = 0;
+bool gotAllRssi = false;
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -87,14 +89,10 @@ class MyClientCallback : public BLEClientCallbacks {
 
   void onDisconnect(BLEClient* pclient) {
     connected = false;
+    // doConnect = true;
     Serial.println("onDisconnect");
   }
 };
-
-static void positionNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  positionStr = (char*)pData;
-  newPosition = true;
-}
 
 // double rssiToDistance(int rssi) {
 //     // Implement path loss model here
@@ -113,7 +111,7 @@ static void positionNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteri
 //     distanceCornerNode3 = rssiToDistance(rssiCornerNode3);
 
 //     // Trilateration algorithm
-//     // Solve equations based on distances and known positions of corner nodes
+//     // Solve equations based on distances and known nodes of corner nodes
 //     // to estimate the coordinates of the asset node
 //     // Example algorithm:
 //     // Calculate intermediate values for trilateration equations
@@ -130,6 +128,31 @@ static void positionNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteri
 //     yAssetNode = (E - A * xAssetNode) / B;
 // }
 
+bool getNodeAddresses(BLEScan* pBLEScan) {
+  Serial.println("Searching for corner nodes...");
+  while (cornerNodesDiscovered < MAX_NODES) {
+    BLEScanResults foundDevices = pBLEScan->start(5, false); // 5,false
+    for (int i = 0; i < foundDevices.getCount(); i++) {
+      BLEAdvertisedDevice res = foundDevices.getDevice(i);
+      if (res.getName() == cornerNodes[cornerNodesDiscovered]) {
+        cornerNodeRssiArr[cornerNodesDiscovered] = res.getRSSI();
+        pServerAddresses[cornerNodesDiscovered] = new BLEAddress(res.getAddress());
+        cornerNodesDiscovered++;
+      }
+    }
+  }
+  gotAllRssi = true;
+  cornerNodesDiscovered = 0;
+  Serial.println("Found all corner nodes!");
+  return true;
+}
+
+static void nodeNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  nodeStr = (char*)pData;
+  Serial.println(nodeStr);
+  newNode = true;
+}
+
 void setup() {
   Serial.begin(115200);
   M5.begin();
@@ -141,46 +164,25 @@ void setup() {
   BLEDevice::init("");
 
   BLEScan* pBLEScan = BLEDevice::getScan();
-
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);
   pBLEScan->setActiveScan(true);
 
-  BLEScanResults foundDevices = pBLEScan->start(5, false); // 5,false
-  Serial.printf("foundDevices.getCount()= %i \n", foundDevices.getCount());
-
-  for (int i = 0; i < foundDevices.getCount(); i++) {
-    if (!isAllNodesDiscovered) {
-      BLEAdvertisedDevice res = foundDevices.getDevice(i);
-      if (
-        res.getName() == cornerNodes[0] ||
-        res.getName() == cornerNodes[1] ||
-        res.getName() == cornerNodes[2]
-      ) {
-        Serial.print(res.getName().c_str());
-        Serial.print(" @ ");
-        Serial.println(res.getAddress().toString().c_str());
-        pServerAddresses[currNodeIndex] = new BLEAddress(res.getAddress());
-        currNodeIndex++;
-      }
-      if (currNodeIndex == 3) {
-        currNodeIndex = 0;
-        isAllNodesDiscovered = true;
-        doConnect = true;
-      }
-    }    
+  if (getNodeAddresses(pBLEScan)) {
+    currNodeIndex = 0;
+    doConnect = true;
   }
 }
 
 void printReadings() {
-  M5.Lcd.setCursor(0, 20, 2);
-  M5.Lcd.print("Position = ");
-  M5.Lcd.print(positionStr);
+  // M5.Lcd.setCursor(0, 20, 2);
+  // M5.Lcd.print("Node = ");
+  // M5.Lcd.print(nodeStr);
 
   // Print corner node RSSI
   M5.Lcd.setCursor(0, 40, 2);
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < MAX_NODES; i++) {
     M5.Lcd.print("Corner Node ");
     M5.Lcd.print(i + 1);
     M5.Lcd.print(" = ");
@@ -202,28 +204,26 @@ bool connectToServer(BLEAddress pAddress, int index) {
     return (false);
   }
 
-  cornerNodeRssiArr[index] = pClient->getRssi();
- 
-  // positionCharacteristic = pRemoteService->getCharacteristic(positionCharacteristicUUID);
+  nodeCharacteristic = pRemoteService->getCharacteristic(nodeCharacteristicUUID);
+  if (nodeCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID");
+    return false;
+  }
+  Serial.println(" - Found our characteristics");
+  nodeCharacteristic->registerForNotify(nodeNotifyCallback);
 
-  // if (positionCharacteristic == nullptr) {
-  //   Serial.print("Failed to find our characteristic UUID");
-  //   return false;
-  // }
-  // Serial.println(" - Found our characteristics");
- 
-  // positionCharacteristic->registerForNotify(positionNotifyCallback);
+  pNodes[index] = pClient;
 
   return true;
 }
 
 void loop() {
   if (doConnect) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < MAX_NODES; i++) {
       if (connectToServer(*pServerAddresses[i], i)) {
         Serial.print("Connected to the BLE Server: "); Serial.println(pServerAddresses[i]->toString().c_str());
         // Activate the Notify property of each Characteristic
-        // positionCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+        nodeCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
       } else {
         Serial.println("Failed to connect to the server; Restart device to scan for nearby BLE server again.");
       }
@@ -231,13 +231,32 @@ void loop() {
     connected = true;
     doConnect = false;
   } 
-
-  if (connected) {
-    // Check if new position data is available
-    // if (newPosition) {
-        // newPosition = false;
+  else {
+    if (connected) {
+      if (gotAllRssi) {
         printReadings();
-    // }
+        gotAllRssi = false;
+      } 
+      else {
+        for (int i = 0; i < MAX_NODES; i++) {
+          pNodes[i]->disconnect();
+          Serial.print("Disonnected to the BLE Server: "); Serial.println(pServerAddresses[i]->toString().c_str());
+        }
+        connected = false;
+      }
+    } 
+    else {
+      BLEScan* pBLEScan = BLEDevice::getScan();
+      pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+      pBLEScan->setInterval(100);
+      pBLEScan->setWindow(99);
+      pBLEScan->setActiveScan(true);
+
+      if (getNodeAddresses(pBLEScan)) {
+        currNodeIndex = 0;
+        doConnect = true;
+      }
+    }
   }
     
   delay(1000);  // Delay one second between loops.
--- T29_Smart_Tracker/CornerNode/CornerNode.ino
+++ T29_Smart_Tracker/CornerNode/CornerNode.ino
@@ -5,13 +5,12 @@
 #define SERVICE_UUID "01234567-0123-4567-89ab-0123456789ab"
 #define bleServerName "CornerNode3"
 
-BLECharacteristic positionCharacteristics("01234567-0123-4567-89ab-0123456789cd", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-BLEDescriptor positionDescriptor(BLEUUID((uint16_t)0x2902));
+BLECharacteristic nodeCharacteristics("01234567-0123-4567-89ab-0123456789cd", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLEDescriptor nodeDescriptor(BLEUUID((uint16_t)0x2902));
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 15000;
+unsigned long timerDelay = 10000;
 bool deviceConnected = false;
-bool ledStatus = false;  // Initial LED status
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -30,7 +29,8 @@ void setup() {
   M5.Lcd.setRotation(3);
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(0, 0, 2);
-  M5.Lcd.printf(bleServerName, 0);
+  M5.Lcd.println(bleServerName);
+  M5.Lcd.println("Waiting for client connection...");
 
   BLEDevice::init(bleServerName);
 
@@ -39,9 +39,9 @@ void setup() {
 
   BLEService *bleService = pServer->createService(SERVICE_UUID);
 
-  bleService->addCharacteristic(&positionCharacteristics);
-  positionCharacteristics.setValue("Node Position");
-  positionCharacteristics.addDescriptor(&positionDescriptor);
+  bleService->addCharacteristic(&nodeCharacteristics);
+  nodeCharacteristics.setValue("Node");
+  nodeCharacteristics.addDescriptor(&nodeDescriptor);
 
   bleService->start();
 
@@ -53,28 +53,25 @@ void setup() {
 
 void loop() {
   BLEDevice::startAdvertising();
-
   if (deviceConnected) {
     if ((millis() - lastTime) > timerDelay) {
-      // Simulate changing position data
-      static int xPos = 0;
-      static int yPos = 0;
-      static int zPos = 0;
-      xPos++;
-      yPos++;
-      zPos++;
-      String positionData = String(xPos) + ":" + String(yPos) + ":" + String(zPos);
-      positionCharacteristics.setValue(positionData.c_str());
-      positionCharacteristics.notify();
+      nodeCharacteristics.setValue(bleServerName);
+      nodeCharacteristics.notify();
       
-      // Display position data on the LCD
-      M5.Lcd.fillScreen(TFT_BLACK);
+      // Display node data on the LCD
+      M5.Lcd.fillScreen(BLACK);
       M5.Lcd.setCursor(0, 0);
-      M5.Lcd.printf("Position: %s", positionData.c_str());
+      M5.Lcd.println(bleServerName);
+      M5.Lcd.println("Connected to a client!");
 
       lastTime = millis();
     }
+  } else {
+    // Display node data on the LCD
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println(bleServerName);
+    M5.Lcd.println("Waiting for client connection...");
   }
-
-  delay(100);  // Delay for stability
+  delay(100);
 }
